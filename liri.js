2c25a05427d4f75e6ac5df91ebfceb8897cea7a0
Leon Lin <leonlin310@gmail.com>
2019-05-14 21:35:08 -0500

    working spotify functionality

--- liri.js
+++ liri.js
@@ -4,14 +4,46 @@ const keys = require("./keys.js");
 
 const Spotify = require('node-spotify-api');
 const spotify = new Spotify(keys.spotify);
+const command = process.argv[2];
+
+// node liri.js spotify-this-song '<song name here>'
+// This will show the following information about the song in your terminal/bash window
+// Artist(s)
+// The song's name
+// A preview link of the song from Spotify
+// The album that the song is from
+// If no song is provided then your program will default to "The Sign" by Ace of Base.
  
+if (command == 'spotify-this-song'){
+    let spotifyResult = process.argv[3];
+        console.log("*********************************************************************************")
+        console.log("Here are the search results for song:", spotifyResult);
+        console.log("*********************************************************************************")
+
+    for (let i = 0; i<3; i++){
+
+    spotify.search({ type: 'track', query: spotifyResult}, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+    //   console.log(data.tracks.items[0]);
+        //TODO: Artist
+        console.log("=================================================================================")
+        console.log("The bands name is: ",data.tracks.items[i].artists[0].name);
+        console.log("The songs name is: ",data.tracks.items[i].name);
+        let nuller = JSON.stringify(data.tracks.items[i].preview_url)
+        if (nuller === "null"){
+            console.log("Click here for a preview of the song: Sorry, but there is no preview available")
+
+        }
+        else {
+        console.log("Click here for a preview of the song:", data.tracks.items[i].preview_url)
+        }
+      });
+    }
+}
 
 
- 
-// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
- 
-// // console.log(data); 
-// });
+
+
+//TODO: working spotify.search
